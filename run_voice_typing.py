e1fe1494a4d006e9ba47f6731fa5873af83ea556
Manvir Heer <manvirheer007@gmail.com>
2025-10-10 04:40:00 -0700

    Tasks: add entry point scripts

---
+++ run_voice_typing.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Simple launcher script for Whisper Voice Typing"""
+
+from whisper_voice_typing import WhisperVoiceTyping
+
+
+if __name__ == "__main__":
+    app = WhisperVoiceTyping()
+    app.run()
